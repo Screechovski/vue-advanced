25d83c7308b2868d731825e17c474bc7786041f7
Yaroslav <webdev1232@gmail.com>
2021-08-28 21:24:51 +0300

    va lesson-1 start

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const proxy = {
+	'/randomapi2': {
+		target: 'http://faceprog.ru',
+		secure: false,
+		changeOrigin: true
+	}
+};
+
+module.exports = {
+	filenameHashing: false,
+	productionSourceMap: false,
+	configureWebpack: config => {
+		return {
+			devServer: { proxy }
+		}
+	}
+}
